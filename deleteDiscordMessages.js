ce462a6b216000e63d21a1aaf5603edbdc3edd13
Vatech <41109879+Vatech1608@users.noreply.github.com>
2020-12-22 15:21:11 +0300

    Update deleteDiscordMessages.js

--- deleteDiscordMessages.js
+++ deleteDiscordMessages.js
@@ -233,9 +233,9 @@
                     searchDelay += w; // increase delay
                     log.warn(`Being rate limited by the API for ${w}ms! Increasing search delay...`);
                     printDelayStats();
-                    log.verb(`Cooling down for ${w * 2}ms before retrying...`);
-                    
-                    await wait(w*2);
+                    //Better result
+                    log.verb(`Cooling down for ${w * 1}ms before retrying...`);
+                    await wait(w*1);
                     return await recurse();
                 } else {
                     return log.error(`Error searching messages, API responded with status ${resp.status}!\n`, await resp.json());
